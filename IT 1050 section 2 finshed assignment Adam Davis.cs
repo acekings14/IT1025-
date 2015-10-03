df477bda6b9b9cda8e141063ed27e6c9b7a7356c
acekings14 <a3davis6094@yahoo.com>
2015-10-03 01:23:07 -0400

    finish
    finish

---
+++ IT 1050 section 2 finshed assignment Adam Davis.cs
@@ -0,0 +1,29 @@
+﻿
+namespace IT_1050_Section_2assignmentAdam_davis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person person1 = new Person();
+            person1.AskforNameAge();
+            person1.AskifMArried();
+
+            Person person2 = new Person();
+            person2.AskforNameAge();
+            person2.AskifMArried();
+
+
+            Person person3 = new Person();
+            person3.AskforNameAge();
+            person3.AskifMArried();
+
+
+            System.Console.WriteLine("Average age" + Person. AverageAge());
+
+
+
+        }
+    }
+}
